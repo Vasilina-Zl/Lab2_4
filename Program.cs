00e662c5c166fa11cc0b5aa3518f90a9e0ccae2b
Vasilina-Zl <terisharkis@gmail.com>
2022-02-13 14:52:20 +0200

    first

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labor2_4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            User Vasylyna = new User("teri@gmail.com", "Vasylyna", "Zlenko", 19);
+
+            (string login, string name, string lastName, int age) = Vasylyna;
+
+            DateTime thisDate1 = new DateTime(2022, 2, 13, 13, 44, 0, DateTimeKind.Local);
+
+
+            Console.WriteLine("Анкета 1:\n" +
+                $"Логiн: {login}\n" +
+                $"Iм'я: {name}\n" +
+                $"Прiзвище: {lastName}\n" +
+                $"Вiк: {age}\n"+
+                "Час створення анкети: {0:dd/MM/yy H:mm zzz}", thisDate1);
+            Console.WriteLine(" \n" +
+                "===============\n" +
+                " ");
+
+            User Jane = new User("aaaaa@gmail.com", "Jane", "Lock", 13);
+
+            (string jlogin, string jname, string jlastName, int jage) = Jane;
+
+            DateTime thisDate2 = new DateTime(2022, 2, 13, 14, 06, 0, DateTimeKind.Local);
+
+
+            Console.WriteLine("Анкета 2:\n" +
+                $"Логiн: {jlogin}\n" +
+                $"Iм'я: {jname}\n" +
+                $"Прiзвище: {jlastName}\n" +
+                $"Вiк: {jage}\n" +
+                "Час створення анкети: {0:dd/MM/yy H:mm zzz}", thisDate2);
+            Console.ReadKey();
+                              
+
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labor2_4
+{
+	internal class User
+	{
+		string login;
+		string name;
+		string lastName;
+		int age;
+
+		public User(string login, string name, string lastName, int age)
+		{
+			this.login = login;
+			this.name = name;
+			this.lastName = lastName;
+			this.age = age;
+
+		}
+		public void Deconstruct(out string uLogin, out string uName, out string uLastName, out int uAge)
+		{
+			uLogin = login;
+			uName = name;
+			uLastName = lastName;
+			uAge = age;
+		}
+	}
+}
